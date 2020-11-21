89c23a8f8e673d473b7ef86892a62c9cbb292cdb
Matt Bidewell <matthewbidewell@gmail.com>
2020-11-21 00:38:10 +0000

    poc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import { createFFmpeg, fetchFile} from "@ffmpeg/ffmpeg"
-const ffmpeg = createFFmpeg({ log: true });
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
+/**
+ * Goal - Upload some audio split the audio on the silences.
+ * 
+ * FFMPEG will log where audio silence starts and ends.
+ * Using a custon logger function we can grab those logs(I dont like it.)
+ * Can turn those logs into instructions to cut the audio.
+ * 
+ * TODO 
+ * - UI
+ * - flush logs (maybe flush on a fresh button press.)
+ */
+
+
+const ffmpeg = createFFmpeg({ log: true, logger: saveLoggerOutput });
+
+let silenceLogs = [];
 
 function App() {
 
     const [ready, setReady] = useState(false);
-    const [video, setVideo] = useState();
+    const [upload, setUpload] = useState();
     const [gif, setGif] = useState();
 
     const load = async () => {
@@ -19,14 +35,62 @@ function App() {
         load();
     }, [])
 
-    const convertToGif = async () => {
-        try {
-            ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video));
-            await ffmpeg.run("-i", "test.mp4", "-t", "2.5", "-ss", "2.0", "-f", "gif", "out.gif");
-            const data = ffmpeg.FS('readFile', 'out.gif');
+    // const convertToGif = async () => {
+    //     try {
+    //         ffmpeg.FS("writeFile", "test.mp4", await fetchFile(upload));
+    //         await ffmpeg.run("-i", "test.mp4", "-t", "2.5", "-ss", "2.0", "-f", "gif", "out.gif");
+    //         const data = ffmpeg.FS('readFile', 'out.gif');
     
-            const url = URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
-            setGif(url);
+    //         const url = URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
+    //         setGif(url);
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // }
+
+    const seperateAudio = async () => {
+        try {
+            silenceLogs = []; // flush logs.
+
+            const mimeType = "ogg" 
+            ffmpeg.FS("writeFile", `upload.${mimeType}`, await fetchFile(upload));
+
+            // run this to get silence logs
+            await ffmpeg.run("-i", "upload.ogg", "-af", "silencedetect=noise=-30dB:d=0.5", "-f", "null", "dummy.txt");
+            let index = 0;
+            const files = [];
+            console.log(silenceLogs)
+
+            // needs to be a sync
+            for (let     [index,silenceLog] of silenceLogs.entries()) {
+                // if its an end log then use it to process the trim
+                console.log(index, silenceLog)
+                if (silenceLog.includes("end") && index !== silenceLogs.length - 1) {
+                    console.log(silenceLog);
+                    //  ffmpeg -ss <silence_end - 0.25> -t <next_silence_start - silence_end + 2 * 0.25> -i input.mov word-N.mov
+                    const silenceEnd = extractSilenceEnd(silenceLog);
+                    const nextSilenceStart = extractNextSilenceStart(silenceEnd);
+                    
+                    const startTime = silenceEnd - 0.25; // need to turn in 00:01:00
+                    const durationTime = nextSilenceStart - silenceEnd + 2 * 0.25;
+
+                    const formattedStart = new Date(startTime * 1000).toISOString().substr(11,12);
+                    const formatedDurationTime = new Date(durationTime * 1000).toISOString().substr(11, 12);
+                    
+                    // const startTime1 = "00:00:08.5859"; // 8.58596 -t 1.8020399999999999
+                    // const durationTime1 = "00:00:01.8020";
+
+                    console.log("command", `ffmpeg -ss ${formattedStart} -t ${formatedDurationTime} -i upload.ogg ${index}.ogg`);
+                    await ffmpeg.run("-i", "upload.ogg", "-ss", formattedStart, "-t", formatedDurationTime, `${index}.ogg`);
+
+                    const data = ffmpeg.FS("readFile", `${index}.ogg`);
+                    const url = URL.createObjectURL(new Blob([data.buffer], { type: "audio/ogg" }));
+                    files.push(url);
+                    index++;
+                }
+            }
+            index = 0;
+            console.log(files);
         } catch (err) {
             console.error(err);
         }
@@ -34,15 +98,37 @@ function App() {
 
     return ready ? (
         <div className="App">
-            {video && <video controls width="250" src={URL.createObjectURL(video)}></video>}
-            <input type="file" onChange={(e) => setVideo(e?.target?.files?.item(0))}></input>
+            {upload && <video controls width="250" src={URL.createObjectURL(upload)}></video>}
+            <input type="file" onChange={(e) => setUpload(e?.target?.files?.item(0))}></input>
 
             <h3>Result</h3>
-            <button onClick={convertToGif}>Convert</button>
+            <button onClick={seperateAudio}>Seperate Audio</button>
 
             {gif && <img src={gif} width="250"></img>}
         </div>
     ) : (<p>Loading...</p>);
+}
+
+function saveLoggerOutput(log) {
+    if (log.message.includes("silencedetect") && !log.message.includes("run")) {
+        silenceLogs.push(log.message);
+    }
+}
+
+// using the end log, gets the next start value.
+function extractNextSilenceStart(silenceEndLog) {
+    const logIndex = silenceLogs.findIndex(log => log.includes(silenceEndLog));
+    const nextStartLog = silenceLogs[logIndex + 1];
+    const [prexix, suffix] = nextStartLog.split("start:");
+    const [number] = suffix.split("|");
+    return Number(number.trim());
+}
+
+// returns a number of the end of a silence
+function extractSilenceEnd(log) {
+    const [prefix, suffix] = log.split("end:");
+    const [number] = suffix.split("|");
+    return Number(number.trim());
 }
 
 export default App;
